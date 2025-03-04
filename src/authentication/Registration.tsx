b0db6f45e431821c20e917043b55982502dbcf77
Miguelkarma <santos.p.miguel46@gmail.com>
2025-03-04 11:28:33 +0800

    Delete AnimatedWrap, modified particles and fixed tailwind of registration

--- src/authentication/Registration.tsx
+++ src/authentication/Registration.tsx
@@ -2,18 +2,17 @@ import { useNavigate } from "react-router-dom";
 import { RegistrationForm } from "@/components/registration-form";
 import logo from "@/assets/logo1.png";
 import back from "@/assets/log.jpg";
-
 import "@/styles/LoginReg.css";
 
 const Registration = () => {
   const navigate = useNavigate();
   return (
-    <div className=" back bg grid min-h-svh lg:grid-cols-2 text-white">
+    <div className=" back grid min-h-svh lg:grid-cols-2 text-white">
       <div className="flex flex-col gap-4 p-6 md:p-10 text-gray-100">
         <div className="flex justify-center gap-2 md:justify-start text-white">
           <a
             href="#"
-            className="flex items-center gap-2 font-medium"
+            className="flex items-center gap-2 font-medium text-xl"
             onClick={(e) => {
               e.preventDefault();
               navigate("/Home");
