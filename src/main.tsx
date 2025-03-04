bdf5fc8206a398ab3294899843a1bd7dee92e193
Miguelkarma <santos.p.miguel46@gmail.com>
2025-03-04 11:20:11 +0800

    added new fonts and new background

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import AppRouter from "./routes/route.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@fontsource/montserrat/400.css";
-import "./index.css";
 import "flowbite";
+import "@fontsource/dm-sans/500.css";
+import "@fontsource/dm-sans/700.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
