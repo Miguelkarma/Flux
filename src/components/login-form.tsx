c68bc69c5a28053bffce5ea25785374036e58f53
Miguelkarma <santos.p.miguel46@gmail.com>
2025-03-05 16:50:50 +0800

    Fixed loader staying when invalid credentials and also planning to add sonner for invalid credentials

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -23,7 +23,7 @@ export function LoginForm({ onLogin, className, ...props }: LoginFormProps) {
     try {
       await onLogin(email, password);
     } finally {
-      setTimeout(() => setLoading(true), 9000);
+      setTimeout(() => setLoading(false), 1000);
     }
   };
 
