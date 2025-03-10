86f2d9dc31bc72092e4e8f88d9e0863ccb3e3377
Miguelkarma <santos.p.miguel46@gmail.com>
2025-03-10 21:32:49 +0800

    delete mock cause of conflicts

--- jest.setup.ts
+++ jest.setup.ts
@@ -5,17 +5,3 @@ import { TextEncoder, TextDecoder as NodeTextDecoder } from "util";
 globalThis.TextEncoder = TextEncoder as typeof globalThis.TextEncoder;
 globalThis.TextDecoder =
   NodeTextDecoder as unknown as typeof globalThis.TextDecoder;
-
-  class MockResizeObserver {
-  observe() {}
-  unobserve() {}
-  disconnect() {}
-}
-Object.defineProperty(globalThis, "ResizeObserver", {
-  writable: true,
-  configurable: true,
-  value: MockResizeObserver,
-});
-jest.mock("lucide-react", () => ({
-  Check: () => "MockedCheckIcon",
-}));
