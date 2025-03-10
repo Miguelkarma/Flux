01be58615728df2626a7d887bbaeca7719cc4284
Miguelkarma <santos.p.miguel46@gmail.com>
2025-03-10 21:38:53 +0800

    re added particles to dashboard

--- src/DashboardPages/Pages/Dashboard/renderDashboard.tsx
+++ src/DashboardPages/Pages/Dashboard/renderDashboard.tsx
@@ -2,10 +2,11 @@ import { AssetOverview } from "./Asset-overview";
 import { AssetAllocation } from "./Asset-allocation";
 import { AssetTable } from "./Asset-table";
 import { RecentActivity } from "./Recent-activity";
-
+import ParticlesBackground from "@/Animation/ParticlesBackground";
 export default function DashboardPage() {
   return (
     <div className="flex flex-grow min-w-0 p-4 transition-all ">
+      <ParticlesBackground/>
       <div className="flex flex-col gap-3 p-2 w-full">
         <AssetOverview />
         <div className="grid gap-6 md:grid-cols-2">
