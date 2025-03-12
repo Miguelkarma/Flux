84f6de4edc83ab54ba8e2659ead4a6ceee47f16d
Miguelkarma <santos.p.miguel46@gmail.com>
2025-03-12 11:24:09 +0800

    re added imports

--- src/DashboardPages/Pages/ExternalAPI.tsx
+++ src/DashboardPages/Pages/ExternalAPI.tsx
@@ -1,3 +1,8 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import Sidebar from "@/components/Sidebar";
+import ParticlesBackground from "@/Animation/ParticlesBackground";
+import { useAuth } from "@/hooks/use-auth";
+
 export default function ExternalAPI() {
   return <></>;
 }
