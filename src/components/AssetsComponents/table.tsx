83e6a8b08a8adebc32a3b9501fa6dc5775743274
Miguelkarma <santos.p.miguel46@gmail.com>
2025-03-17 09:24:52 +0800

    accidentally merged changes from another branch

--- src/components/AssetsComponents/table.tsx
+++ src/components/AssetsComponents/table.tsx
@@ -41,7 +41,6 @@ import { Card } from "../ui/card";
 import TableLoader from "@/Animation/TableLoader";
 import { BulkDeleteComponent } from "./BulkDeleteDialog";
 import { ScrollArea, ScrollBar } from "../ui/scroll-area";
-import { UploadFile } from "./UploadFile";
 
 type NewType = {
   id: string;
@@ -194,15 +193,6 @@ export function DataTable() {
               }
             }}
             userEmail={userEmail}
-          />
-          <UploadFile
-            onAssetsAdded={() => {
-              const auth = getAuth();
-              const user = auth.currentUser;
-              if (user) {
-                fetchAssets(user.uid);
-              }
-            }}
           />
 
           {/* Bulk Delete */}
