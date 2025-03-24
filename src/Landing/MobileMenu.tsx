5650e0e3c9462ed3d013a1c7e68a33ce3b7d23ee
toropaulmiguelsantos <toro.paulmiguel.santos@gmail.com>
2025-03-24 12:31:12 +0800

    resolved conflicts

--- src/Landing/MobileMenu.tsx
+++ src/Landing/MobileMenu.tsx
@@ -7,6 +7,16 @@ type MobileMenuProps = {
 };
 
 const MobileMenu = ({ navMenu }: MobileMenuProps) => {
+  const handleScroll = (
+    e: React.MouseEvent<HTMLAnchorElement>,
+    href: string
+  ) => {
+    e.preventDefault();
+    const target = document.querySelector(href);
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <>
       <div>
@@ -14,7 +24,12 @@ const MobileMenu = ({ navMenu }: MobileMenuProps) => {
           {navMenu.map(({ href, label }, index) => (
             <li key={index}>
               <Button asChild variant="ghost" className="w-full justify-start">
-                <a href={href || "#"}>{label}</a>
+                <a
+                  href={href || "#"}
+                  onClick={(e) => handleScroll(e, href || "#")}
+                >
+                  {label}
+                </a>
               </Button>
             </li>
           ))}
@@ -25,8 +40,12 @@ const MobileMenu = ({ navMenu }: MobileMenuProps) => {
           <Button variant="ghost" className="w-full">
             Sign In
           </Button>
-          <Button className="relative w-full  text-white bg-black border border-white/30 rounded-full transition-all hover:bg-black 
-                before:absolute before:left-1/2 before:translate-x-[-50%] before:bottom-[-2px] before:w-[66%] before:h-[4px] before:bg-gradient-to-r before:from-transparent before:via-teal-500 before:to-transparent hover:border-teal-200 before:rounded-full">Log In</Button>
+          <Button
+            className="relative w-full  text-white bg-black border border-white/30 rounded-full transition-all hover:bg-black 
+                before:absolute before:left-1/2 before:translate-x-[-50%] before:bottom-[-2px] before:w-[66%] before:h-[4px] before:bg-gradient-to-r before:from-transparent before:via-teal-500 before:to-transparent hover:border-teal-200 before:rounded-full"
+          >
+            Log In
+          </Button>
         </div>
       </div>
     </>
