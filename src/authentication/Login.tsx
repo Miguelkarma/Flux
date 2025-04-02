39d760a2e87c98e2572e4e07168c5042102c985a
Miguelkarma <santos.p.miguel46@gmail.com>
2025-04-02 16:34:20 +0800

    added login desgign

--- src/authentication/Login.tsx
+++ src/authentication/Login.tsx
@@ -56,38 +56,39 @@ function Login() {
         expand={true}
         visibleToasts={3}
       />
-      <div className="back grid min-h-svh lg:grid-cols-2 text-white">
-        <div className="flex flex-col gap-4 p-6 md:p-10 text-gray-100">
-          <div className="flex justify-center gap-2 md:justify-start text-white">
-            <a
-              href="#"
-              className="flex items-center gap-2 font-medium text-xl"
-              onClick={(e) => {
-                e.preventDefault();
-                navigate("/Home");
-              }}
-            >
-              <Waypoints className="h-8 w-8 text-teal-300" />
-              <div className="text-2xl font-bold bg-gradient-to-t from-teal-200 via-teal-400 to-cyan-800 bg-clip-text text-transparent">
-                Flux
-              </div>
-            </a>
+
+      <div className="bg-gradient-to-r from-gray-900 via-teal-950 to-teal-900 shadow-xl rounded-lg overflow-hidden w-screen h-screen grid grid-cols-1 lg:grid-cols-2">
+        {/* Left Side - Login Form */}
+        <div className="p-10 flex flex-col justify-center ">
+          {/* Logo & Title */}
+          <div className="flex items-center gap-2 mb-6 ">
+            <Waypoints className="h-8 w-8 text-teal-300" />
+            <span className="text-2xl font-bold bg-gradient-to-r from-teal-300 to-teal-400 bg-clip-text text-transparent">
+              Flux
+            </span>
           </div>
-          <div className="flex flex-1 items-center justify-center">
-            <div className="w-full max-w-xs ">
-              <LoginForm onLogin={handleLogin} />
-            </div>
-          </div>
+
+          <h2 className="text-2xl font-semibold text-white">Welcome back!</h2>
+          <p className="text-gray-500 text-sm mb-6">
+            Enter your details below.
+          </p>
+
+          {/* Login Form */}
+          <LoginForm onLogin={handleLogin} />
         </div>
-        <div className="relative hidden bg-muted lg:block">
+
+        {/* Right Side - Image */}
+        <div className="hidden lg:block relative bg-mask opacity-100">
           <img
             src={back}
-            alt="Image"
-            className="absolute inset-0 h-full w-full object-cover"
+            alt="Login Illustration"
+            className="absolute w-full h-full object-cover "
+            style={{ backgroundPosition: "left bottom" }}
           />
         </div>
       </div>
     </>
   );
 }
+
 export default Login;
